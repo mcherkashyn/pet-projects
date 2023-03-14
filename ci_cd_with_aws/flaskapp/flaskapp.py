f3aa4d9ef6a80bad56605eb8bd7ad1ffbe9a2904
mcherkashyn <mcherkashinrc@gmail.com>
2023-03-14 18:34:54 +0200

    test ci/cd

--- ci_cd_with_aws/flaskapp/flaskapp.py
+++ ci_cd_with_aws/flaskapp/flaskapp.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    some_change = "some new code edit"
+    some_change = "some new code"
     return f"Hello, World!, {some_change}"
 
 if __name__ == "__main__":
