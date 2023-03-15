d4bbd2a8de363ef4f0b6b3136bb86992d91597d0
mcherkashyn <mcherkashinrc@gmail.com>
2023-03-15 04:08:08 +0200

    test ci/cd

--- ci_cd_with_aws/main.tf
+++ ci_cd_with_aws/main.tf
@@ -1,14 +1,14 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+
 terraform {
   backend "s3" {
     bucket = "tfbackend434"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
-}
-
-
-provider "aws" {
-  region = var.aws_region
 }
 
 
