1430a9ce44066c470568c719ea5ba19f14c71474
mcherkashyn <mcherkashinrc@gmail.com>
2023-03-15 04:58:25 +0200

    test ci/cd

--- ci_cd_with_aws/backend/backend.tf
+++ ci_cd_with_aws/backend/backend.tf
@@ -2,7 +2,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.bucket_name
   versioning {
--- ci_cd_with_aws/backend/variables.tf
+++ ci_cd_with_aws/backend/variables.tf
@@ -1,7 +1,9 @@
 variable "aws_region" {
+        description = "AWS region"
         default = "us-east-1"
 }
 
 variable "bucket_name" {
+        type = string
         default = "tfbackend434"
 }
