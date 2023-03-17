44a29448f6837388e3fc64c92121555405cf927c
mcherkashyn <mcherkashinrc@gmail.com>
2023-03-17 02:21:48 +0200

    edit docker userdata

--- docker_with_aws/main.tf
+++ docker_with_aws/main.tf
@@ -122,13 +122,10 @@ sudo apt-get update
 sudo apt install git
 sudo apt install apt-transport-https ca-certificates curl gnupg lsb-release -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io -y
-
-
 sudo mkdir //project && cd /project
 sudo git clone https://github.com/mcherkashyn/pet-projects.git
 cd pet-projects/docker_with_aws/web_app/
