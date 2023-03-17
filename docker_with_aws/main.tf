044a32b6ede17254db7f4f51935d1398d759260f
mcherkashyn <mcherkashinrc@gmail.com>
2023-03-17 02:28:56 +0200

    edit docker userdata

--- docker_with_aws/main.tf
+++ docker_with_aws/main.tf
@@ -120,10 +120,9 @@ resource "aws_instance" "tf_ec2_instance" {
 #!/bin/bash
 sudo apt-get update
 sudo apt install git
-sudo apt install apt-transport-https ca-certificates curl gnupg lsb-release -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt install apt-transport-https ca-certificates curl gnupg lsb-release -y +
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg +
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io -y
 sudo mkdir //project && cd /project
