6bd1c7193528116b27189c7ffef62559511e619d
mcherkashyn <mcherkashinrc@gmail.com>
2023-03-21 10:34:10 +0200

    edit Jenkinsfile

--- ci_cd_with_jenkins/user_data.sh
+++ ci_cd_with_jenkins/user_data.sh
@@ -7,7 +7,6 @@ sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmo
 sudo echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt install docker-ce docker-ce-cli containerd.io -y
-sudo apt-get update
 
 #install Jenkis
 sudo apt install openjdk-17-jre -y
