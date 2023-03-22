35fe31cc2e4dc7b0037fdc2cb68a032c685872f2
mcherkashyn <mcherkashinrc@gmail.com>
2023-03-22 04:56:36 +0200

    add new ci/cd workflow

---
+++ ci_cd_with_aws_ecs/user_data.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#install Docker
+sudo apt-get update
+sudo apt install apt-transport-https ca-certificates curl gnupg lsb-release -y
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt install docker-ce docker-ce-cli containerd.io -y
+
+#install Jenkis
+sudo apt install openjdk-17-jre -y
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
+sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt install jenkins -y
+sudo sed -i '16 i\PATH=/bin:/usr/bin:/sbin:/usr/sbin:/usr/lib/jvm/java-17-openjdk-amd64/bin/' /etc/init.d/jenkins
+sudo usermod -aG docker jenkins
+sudo service jenkins start
---
+++ ci_cd_with_aws_ecs/variables.tf
@@ -0,0 +1,48 @@
+variable "aws_region" {
+        default = "us-east-1"
+}
+
+variable "subnet_count" {
+        description = "Number of subnets"
+        type = map(number)
+        default = {
+                public = 1,
+                private = 1,
+        }
+}
+
+variable "vpc_cidr_block" {
+        description = "CIDR block for vpc"
+        default = "172.20.0.0/16"
+}
+
+variable "public_subnet_cidr_blocks" {
+        description = "CIDR blocks for public subnets"
+        type = list(string)
+        default = [
+                "172.20.1.0/24",
+                "172.20.2.0/24"
+        ]
+}
+
+variable "private_subnet_cidr_blocks" {
+        description = "CIDR blocks for private subnets"
+        type = list(string)
+        default = [
+                "172.20.3.0/24",
+                "172.20.4.0/24"
+        ]
+}
+
+variable "settings" {
+        description = "Configuration settings"
+        type = map(any)
+        default = {
+                "ec2_instance" = {
+                        count = 1
+                        ami = "ami-0557a15b87f6559cf"
+                        instance_type = "t2.micro"
+                        key_name = "test-key-pair"
+                }
+        }
+}
