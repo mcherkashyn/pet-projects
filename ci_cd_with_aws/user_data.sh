a5ba1a41c4f93624dab29ffaa931e81a99d7f650
mcherkashyn <mcherkashinrc@gmail.com>
2023-03-24 09:09:12 +0200

    edit files

--- ci_cd_with_aws/user_data.sh
+++ ci_cd_with_aws/user_data.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+#install Python, Apache
 sudo apt-get update
 sudo apt install git
 sudo apt-get install python3
@@ -7,7 +9,6 @@ sudo apt install apache2 -y
 sudo apt-get install libapache2-mod-wsgi-py3
 sudo pip3 install Flask
 sudo mv /etc/apache2/sites-enabled/000-default.conf /etc/apache2/sites-enabled/000-default.conf.old
-sudo mkdir //project && cd /project
 sudo git clone https://github.com/mcherkashyn/pet-projects.git
 cd pet-projects/ci_cd_with_aws/flaskapp
 sudo mv apache2_config.conf /etc/apache2/sites-enabled/
