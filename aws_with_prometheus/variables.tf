328ab07309cc97812a981a199aba1800051156a1
mcherkashyn <cherkashyn.mykhailo@gmail.com>
2023-10-20 04:19:19 +0300

    removed old files

--- aws_with_prometheus/variables.tf
+++ aws_with_prometheus/variables.tf
@@ -30,14 +30,10 @@ variable "public_subnets" {
         default     = ["10.0.101.0/24"]
 }
 
-#,, "10.0.102.0/24" "10.0.103.0/24"
-
 variable "private_subnets" {
         description = "CIDR blocks for private subnets"
         default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
-
-#, "10.0.2.0/24", "10.0.3.0/24"
 
 variable "master_ec2" {
         description = "Master EC2 instance name"
